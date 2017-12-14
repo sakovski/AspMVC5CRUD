03b218ac41ce28d2d30eb3d68aafc4abb067f37f
Seweryn Rutkowski <sakasew@gmail.com>
2017-12-14 12:27:02 +0100

    first commit, login and register page done

---
+++ Sklep/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using Sklep.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sklep.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            using(MyDbContext context = new MyDbContext())
+            {
+                return View(context.userAccount.ToList());
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if(ModelState.IsValid)
+            {
+                using(MyDbContext context = new MyDbContext())
+                {
+                    context.userAccount.Add(account);
+                    context.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = "Welcome " + account.FirstName + " " + account.LastName + "! You're succesfully registered!";
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount account)
+        {
+            using (MyDbContext context = new MyDbContext())
+            {
+                var user = context.userAccount.Single(u => u.Username == account.Username && u.Password == account.Password);
+                if(user != null)
+                {
+                    Session["UserID"] = user.userID.ToString();
+                    Session["UserName"] = user.Username.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password is wrong.");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ Sklep/Models/UserAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sklep.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int userID { get; set; }
+
+        [Required(ErrorMessage="First name is required!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required!")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required!")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage="you need to confirm your password!")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
